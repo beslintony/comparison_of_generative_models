40608eee9ecf03eca55a0ab53c2daddebb2f2c5c
beslintony <83027084+beslintony@users.noreply.github.com>
2024-01-19 12:40:23 +0100

    Fix fid

--- model_eval.py
+++ model_eval.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from scipy.linalg import sqrtm
+from tensorflow_gan import eval as tfgan_eval
 import ot 
 
 class ModelEvaluator:
@@ -19,28 +19,12 @@ class ModelEvaluator:
 
         for i in range(0, inps.shape[0], self.batch_size):
             inp = inps[i: i + self.batch_size]
-            # inp = tf.keras.applications.inception_v3.preprocess_input(inp)
             act[i: i + self.batch_size] = self._inception_activations(inp).numpy()
 
         return act
 
-    def _calculate_fid(self, activations1, activations2):
-        mu1, sigma1 = np.mean(activations1, axis=0), np.cov(activations1, rowvar=False)
-        mu2, sigma2 = np.mean(activations2, axis=0), np.cov(activations2, rowvar=False)
-
-        diff = mu1 - mu2
-        covmean = sqrtm(sigma1.dot(sigma2))
-
-        # Calculate FID
-        fid = np.sum(diff**2) + np.trace(sigma1 + sigma2 - 2*covmean.real)
-
-        return fid
-
-    def calculate_fid(self, real_images, generated_images):
-        act_real = self._get_inception_activations(real_images)
-        act_generated = self._get_inception_activations(generated_images)
-        fid = self._calculate_fid(act_real, act_generated)
-        return fid
+    def _activations2distance(self, act1, act2):
+        return tfgan_eval.frechet_classifier_distance_from_activations(act1, act2)
 
     def _calculate_kl_divergence(self, p_yx, p_y, eps=1E-16):
         kl_d = p_yx * (tf.math.log(p_yx + eps) - tf.math.log(p_y + eps))
@@ -76,7 +60,6 @@ class ModelEvaluator:
     def calculate_inception_score(self, images, n_split=10):
         model = tf.keras.applications.InceptionV3()
         images = images.astype('float32')
-        # images = tf.keras.applications.inception_v3.preprocess_input(images)
         scores = []
         n_part = images.shape[0] // n_split
         for i in range(n_split):
@@ -94,7 +77,14 @@ class ModelEvaluator:
             scores.append(is_score)
         is_avg, is_std = np.mean(scores), np.std(scores)
         return is_avg, is_std
+    
+    def calculate_fid(self, real_images, generated_images):
+        act_real = self._get_inception_activations(real_images)
+        act_generated = self._get_inception_activations(generated_images)
+        fid = self._activations2distance(act_real, act_generated)
+        return fid
 
     def calculate_wasserstein_distance(self, real_images, generated_images):
         wasserstein_distance = self._calculate_wasserstein_distance(real_images, generated_images)
         return wasserstein_distance
+
